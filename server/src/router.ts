6a666986a2da1d1c0ad74fd8411c1a0561ef33d4
Esteban Velasquez <eavc.30@gmail.com>
2022-05-04 14:36:45 -0500

    fix(back): fix route error
    - queries and mutations were not identified.

--- server/src/router.ts
+++ server/src/router.ts
@@ -1,5 +1,5 @@
+import { z } from 'zod';
 import * as trpc from '@trpc/server';
-import z, { ZodObject } from 'zod';
 
 // define the schema for the request using zod
 const Cat = z.object({
@@ -12,87 +12,86 @@ let cats: Cat[] = [];
 
 const newId = (): number => Math.floor(Math.random() * 100000);
 
-const trpcRouter = trpc.router();
+const trpcRouter = trpc.router()
+  .query('get', {
+    input: z.number(),
+    output: z.object({ cat: Cat, message: z.string() }),
+    async resolve(req) {
+      const cat = cats.find((cat) => cat.id === req.input);
 
-trpcRouter.query('get', {
-  input: z.number(),
-  output: z.object({ cat: Cat, message: z.string() }),
-  async resolve(req) {
-    const cat = cats.find((cat) => cat.id === req.input);
+      if (!cat) {
+        throw new trpc.TRPCError({
+          code: 'NOT_FOUND',
+          message: `Could not find cat with id ${req.input}`,
+        });
+      }
 
-    if (!cat) {
-      throw new trpc.TRPCError({
-        code: 'NOT_FOUND',
-        message: `Could not find cat with id ${req.input}`,
-      });
-    }
+      return { cat, message: `Found cat ${cat.name}` };
+    },
+  })
 
-    return { cat, message: `Found cat ${cat.name}` };
-  },
-});
+  .query('list', {
+    output: z.object({ cats: Cats, total: z.number(), message: z.string() }),
+    async resolve() {
+      if (cats.length === 0) {
+        throw new trpc.TRPCError({
+          code: 'BAD_REQUEST',
+          message: 'No cats found',
+        });
+      }
 
-trpcRouter.query('list', {
-  output: z.object({ cats: Cats, total: z.number(), message: z.string() }),
-  async resolve() {
-    if (cats.length === 0) {
-      throw new trpc.TRPCError({
-        code: 'BAD_REQUEST',
-        message: 'No cats found',
-      });
-    }
+      return { cats, total: cats.length, message: 'Cats found' };
+    },
+  })
 
-    return { cats, total: cats.length, message: 'Cats found' };
-  },
-});
+  .mutation('create', {
+    input: z.object({ name: z.string().max(50) }),
+    output: z.object({ cat: Cat, message: z.string() }),
+    async resolve(req) {
+      const newCat: Cat = { id: newId(), name: req.input.name };
+      cats.push(newCat);
 
-trpcRouter.mutation('create', {
-  input: z.object({ name: z.string().max(50) }),
-  output: z.object({ cat: Cat, message: z.string() }),
-  async resolve(req) {
-    const newCat: Cat = { id: newId(), name: req.input.name };
-    cats.push(newCat);
+      return { cat: newCat, message: 'Cat created' };
+    },
+  })
 
-    return { cat: newCat, message: 'Cat created' };
-  },
-});
+  .mutation('update', {
+    input: z.object({ id: z.number(), name: z.string().max(50) }),
+    output: z.object({ cat: Cat, message: z.string() }),
+    async resolve(req) {
+      const cat = cats.find((cat) => cat.id === req.input.id);
 
-trpcRouter.mutation('update', {
-  input: z.object({ id: z.number(), name: z.string().max(50) }),
-  output: z.object({ cat: Cat, message: z.string() }),
-  async resolve(req) {
-    const cat = cats.find((cat) => cat.id === req.input.id);
+      if (!cat) {
+        throw new trpc.TRPCError({
+          code: 'NOT_FOUND',
+          message: `Could not find cat with id ${req.input.id}`,
+        });
+      }
 
-    if (!cat) {
-      throw new trpc.TRPCError({
-        code: 'NOT_FOUND',
-        message: `Could not find cat with id ${req.input.id}`,
-      });
-    }
+      cat.name = req.input.name;
 
-    cat.name = req.input.name;
+      return { cat, message: 'Cat updated' };
+    },
+  })
 
-    return { cat, message: 'Cat updated' };
-  },
-});
+  .mutation('delete', {
+    input: z.number(),
+    output: z.object({ id: z.number(), message: z.string() }),
+    async resolve(req) {
+      const cat = cats.find((cat) => cat.id === req.input);
 
-trpcRouter.mutation('delete', {
-  input: z.number(),
-  output: z.object({ id: z.number(), message: z.string() }),
-  async resolve(req) {
-    const cat = cats.find((cat) => cat.id === req.input);
+      if (!cat) {
+        throw new trpc.TRPCError({
+          code: 'NOT_FOUND',
+          message: `Could not find cat with id ${req.input}`,
+        });
+      }
 
-    if (!cat) {
-      throw new trpc.TRPCError({
-        code: 'NOT_FOUND',
-        message: `Could not find cat with id ${req.input}`,
-      });
-    }
+      cats = cats.filter((cat) => cat.id !== req.input);
 
-    cats = cats.filter((cat) => cat.id !== req.input);
-
-    return { id: req.input, message: 'Cat deleted' };
-  },
-});
+      return { id: req.input, message: 'Cat deleted' };
+    },
+  });
 
 export type Cat = z.infer<typeof Cat>;
 export type Cats = z.infer<typeof Cats>;
